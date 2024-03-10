93c72a9af049af3450b72495c4d7cac19ee93422
gerli-arce <yoninfantearce@gmail.com>
2024-03-09 20:42:03 -0500

    nts

--- src/main/java/com/yon/backend/cartapp/backendcartapp/auth/filters/JwtAuthenticationFilter.java
+++ src/main/java/com/yon/backend/cartapp/backendcartapp/auth/filters/JwtAuthenticationFilter.java
@@ -1,11 +1,20 @@
 package com.yon.backend.cartapp.backendcartapp.auth.filters;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yon.backend.cartapp.backendcartapp.models.entities.User;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -23,17 +32,60 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
             throws AuthenticationException {
-                return authenticationManager.authenticate(null);
+
+        User user = null;
+        String username = null;
+        String password = null;
+
+        try {
+            user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+            username = user.getUsername();
+            password = user.getPassword();
+            logger.info("Username desde request ImputStream (raw) " + username);
+            logger.info("Password desde request ImputStream (raw) " + password);
+        } catch (StreamReadException e) {
+            e.printStackTrace();
+        } catch (DatabindException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
+
+        return authenticationManager.authenticate(authToken);
     }
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
             Authentication authResult) throws IOException, ServletException {
+
+        String username = ((org.springframework.security.core.userdetails.User) authResult.getPrincipal())
+                .getUsername();
+        String originalInput = "token_." + username;
+        String token = java.util.Base64.getEncoder().encodeToString(originalInput.getBytes());
+
+        response.addHeader("Authorization", "Bearer " + token);
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("token", token);
+        body.put("message", String.format("Hola %s. has iniciado sesion con exito", username));
+        body.put("username", username);
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setStatus(200);
+        response.setContentType("application/json");
     }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException failed) throws IOException, ServletException {
+                Map<String, Object> body = new HashMap<>();
+                body.put("message", "Error en la autenticacion username o password incorrecto");
+                body.put("error", failed.getMessage());
+
+                response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+                response.setStatus(401);
+                response.setContentType("application/json");
     }
 
 }
